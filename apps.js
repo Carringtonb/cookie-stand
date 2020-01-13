21bec5073bb87a2f293cce923d4156a1d177468d
carrington <carringtonbeard@live.com>
2020-01-13 14:31:37 -0800

    created files and tried to start writing code

---
+++ apps.js
@@ -0,0 +1,16 @@
+
+// Creating an array to hold all of the store hours
+var hoursOpen = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm']
+
+function specificStore(storeName, minimumGuests, maximumGuests, avgCookies);{
+    this.minimumGuests
+    this.maximumGuests
+    this.avgCookies
+};
+
+function getRandom(min, max){
+    return (Math.random() * (max - min) - min);
+};
+
+
+
